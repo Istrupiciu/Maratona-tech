9438487858b5b446e2bca4fc6d351b167f6b279a
Istrupiciu <63119693+Esdras10004@users.noreply.github.com>
2023-08-13 12:24:05 -0300

    Add files via upload

---
+++ Maratona-Tech-main/scripts/navegacao.js
@@ -0,0 +1,26 @@
+function navegar(){
+    var plano1 = document.getElementById('card');
+    plano1 = window.open("./planos/plano01.html", "_self");
+}
+
+function navegar2(){
+    var plano1 = document.getElementById('card2');
+    plano1 = window.open("./planos/plano02.html", "_self");
+}
+
+function navegar3(){
+    var plano1 = document.getElementById('card3');
+    plano1 = window.open("./planos/plano03.html", "_self");
+}
+function navegar4(){
+    var plano1 = document.getElementById('card4');
+    plano1 = window.open("./planos/plano04.html", "_self");
+}
+function navegar5(){
+    var plano1 = document.getElementById('card5');
+    plano1 = window.open("./planos/plano05.html", "_self");
+}
+function navegar6(){
+    var plano1 = document.getElementById('card6');
+    plano1 = window.open("./planos/plano06.html", "_self");
+}
